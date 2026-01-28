1228c560f1befcd29653f71bcc26f20d232e6ecf
Malcolm Clayborne <clayborne@chapman.edu>
2026-01-27 23:16:37 -0800

    Committing

---
+++ maxscore.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# maxscore
+# Malcolm Clayborne
+
+scores=()
+
+echo "Enter 5 scores:"
+
+
+read -r scores["0"]
+max=${scores[0]}
+
+for ((i=1;i<5;i++)); do
+	read -r scores["$i"]
+	if (( scores[i] > max ));then
+		max=${scores["$i"]}
+	fi
+done
+
+echo "The highest score is $max"
+echo "The scores are:"
+
+for ((i=0;i<5;i++)); do
+	diff=$(( max - scores["$i"] ))
+	echo "${scores[i]} differs from max by $diff"
+done
